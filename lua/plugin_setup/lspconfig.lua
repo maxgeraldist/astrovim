0c0a6fc76b288b1574d79ab218545acb8e317705
Mike <maxggeraldist@gmail.com>
2025-02-13 15:09:52 -0600

    configs streamlined for faster loadtime

--- lua/plugin_setup/lspconfig.lua
+++ lua/plugin_setup/lspconfig.lua
@@ -6,17 +6,6 @@ return {
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Pyright for Python
-		-- Basedpyright for Python
-		lspconfig.basedpyright.setup({
-			capabilities = capabilities,
-			settings = {
-				python = {
-					analysis = { autoSearchPaths = true, useLibraryCodeForTypes = true },
-					formatting = { provider = "black" },
-				},
-			},
-		})
 		-- Lua
 		lspconfig.lua_ls.setup({})
 
