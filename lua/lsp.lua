370d137f0e394968c7fedb3b5bda9020eba5a44c
maxgeraldist <maxgeraldist@gmail.com>
2025-10-27 01:11:27 -0500

    rewrote texlab lsp setup, minor efficiency improvements

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -61,16 +61,73 @@ cfg("sqlls", {
 });
 vim.lsp.enable("sqlls");
 -- 4. LaTeX
-cfg("texlab", { filetypes = { "tex", "latex", "bibtex" } });
+cfg("texlab", {
+    cmd = { "texlab" },
+    filetypes = { "tex", "latex", "bibtex" },
+    lint = {
+        onChange = true
+    },
+    settings = {
+        texlab = {
+            build = {
+                executable = "latexmk",
+                args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+                onSave = true,
+                forwardSearchAfter = true,
+            },
+            forwardSearch = {
+                executable = "SumatraPDF",
+                -- %l is line number, %f is file path, %p is the PDF file path.
+                args = { "--synctex-forward", "%l:%c:%f", "%p" },
+            },
+            -- Configuration for the ChkTeX linter
+            chktex = {
+                onOpenAndSave = true,
+                onEdit = false,
+            },
+            diagnosticsDelay = 300,
+            formatterLineLength = 88,
+            latexFormatter = "latexindent",
+            bibtexFormatter = "latexindent",
+            latexindent = {
+                ["local"] = nil,
+                modifyLineBreaks = true,
+            },
+            -- Directory to store auxiliary files (.aux, .log, etc.).
+            -- Setting this can keep your main project directory clean.
+            -- Example: "build" or ".aux"
+            auxDirectory = ".",
+            -- A list of BibTeX files to be used for completion and diagnostics.
+            bibtexFiles = {},
+            -- A list of paths to search for bibtex files.
+            bibtexSearchPaths = {},
+            codeActions = {
+                enabled = true,
+            },
+            expl3 = {
+                enabled = true,
+            },
+            forwardSearchDependencies = { "bbl", "cls", "sty" },
+            packageDocSearchPaths = {},
+            packageSearchPaths = {},
+            preview = {
+                enabled = true,
+            },
+            poll = {
+                enabled = true,
+            },
+        },
+    },
+});
 vim.lsp.enable("texlab");
 
 -- 5. LTeX (Grammar checking)
-cfg("ltex", {
-    settings = { ltex = { language = "en-GB" } },
-    filetypes = { "tex", "latex", "bibtex", "markdown" }, -- Also useful for Markdown
-});
-vim.lsp.enable("ltex");
-
+-- cfg("ltex", {
+--     settings = { ltex = { language = "en-GB" } },
+--     filetypes = { "tex", "latex", "bibtex", "markdown" }, -- Also useful for Markdown
+-- });
+-- vim.lsp.enable("ltex");
+--
 -- 6. Remark (Markdown)
 cfg("remark_ls", {
     settings = { remark = { requireConfig = true } },
--- lua/plugin_setup/evil_lualine.lua
+++ lua/plugin_setup/evil_lualine.lua
@@ -205,7 +205,7 @@ ins_right({
 ins_right({
     "diff",
     -- Is it me or the symbol for modified is really weird
-    symbols = { added = " ", modified = "󰝤 ", removed = " " },
+    symbols = { added = " ", modified = "󰝤 ", removed = " " },
     diff_color = {
         added = { fg = colors.green },
         modified = { fg = colors.orange },
